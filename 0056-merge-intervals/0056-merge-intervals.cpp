327516e1b8ef00f8195c4638533cfdaf1bc97c48
Roha <165508799+Rohaatta@users.noreply.github.com>
2024-09-17 23:54:05 +0500

    Time: 31 ms (15.81%), Space: 24 MB (19.95%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+        int s = intervals.size();
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> output;
         for(auto interval : intervals){
