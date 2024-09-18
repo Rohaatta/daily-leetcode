40327d21d077b0efe942a01e0989e42ada2ce724
Roha <165508799+Rohaatta@users.noreply.github.com>
2024-09-18 23:24:50 +0500

    Time: 5 ms (21.12%), Space: 8.3 MB (21.32%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -3,19 +3,19 @@ public:
     bool isHappy(int n) {
         unordered_set<int>s;
         while(true){
-            int summ=0;
+            int sum=0;
             while(n!=0){
                 int digit=n%10;
-                summ+=digit*digit;
+                sum+=digit*digit;
                 n=n/10;
             }
 
-            if(summ==1)return true;
+            if(sum==1)return true;
 
-            if(s.find(summ)!=s.end())return false;
-            s.insert(summ);
+            if(s.find(sum)!=s.end())return false;
+            s.insert(sum);
 
-            n=summ;
+            n=sum;
         }
     }
 };
